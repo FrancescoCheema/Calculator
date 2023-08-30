25c4181dfdd0ed444ee08de43a4974f63aa06e58
FrancescoCheema <cekkosingh@gmail.com>
2023-08-30 00:00:42 -0700

    css styles & functions in js

--- main.js
+++ main.js
@@ -1,19 +1,48 @@
+/*set values to '' start off with */
 let previousNumber = '';
 let currentNumber = '';
 let operator = '';
 
-let previousScreen = document.querySelector('.previous');
-let currentScreen = document.querySelector('.current');
+/*set values and query respective HTML elements */
+let previousScreen = document.querySelector('.previous-number');
+let currentScreen = document.querySelector('.current-number');
 let equal = document.querySelector('.equal');
 let decimal = document.querySelector('.decimal');
-let clearBtn = document.querySelector('#clear-btn');
-let numbers = document.querySelector('.number');
-let operators = document.querySelector('.operator');
+let clearBtn = document.querySelector('.clear');
+let numbers = document.querySelectorAll('.number');
+let operators = document.querySelectorAll('.operator');
 
-numbers.forEach((number) => number.addEventListener("click", function(e){
+/*query all numbers' buttons, if a buttons is clicked, the number is returned on screen*/
+numbers.forEach((num) => num.addEventListener("click", function(e){
     handleNumber(e.target.textContent)
+    currentScreen.textContent = currentNumber;
 }));
 
+operators.forEach((operator) => operator.addEventListener("click", function(e){
+    handleOperator(e.target.textContent)
+}));
+
+equal.addEventListener("click", handleEqual())
+
 function handleNumber(num){
-    console.log(num);
+    currentNumber += num;
 }
+
+function handleOperator(operator){
+    currentScreen.textContent = currentNumber + operator;
+}
+
+function handleEqual(operator){
+    previousNumber = Number(previousNumber);
+    currentNumber = Number(currentNumber);
+
+    if(operator === "+"){
+        previousNumber += currentNumber;
+    } else if(operator === "-"){
+        previousNumber -= currentNumber;
+    } else if(operator === "x"){
+        previousNumber *= currentNumber;
+    } else if (operator === "/"){
+        previousNumber /= currentNumber;
+    }
+};
